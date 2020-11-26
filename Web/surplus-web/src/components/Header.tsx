bebd378e60cb6e6735fde9620e8aed91b8947488
JeremyKrasin <33Infinity@gmail.com>
2020-11-26 15:01:40 -0800

    Added nav bar using material ui

--- Web/surplus-web/src/components/Header.tsx
+++ Web/surplus-web/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBarMain from "./NavBarMain";
 
-const Header = () => <NavBarMain />;
+const Header = () => <NavBarMain NotificationCount="3" />;
 
 export default Header;

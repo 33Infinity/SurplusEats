d88097a4d79ab1e2e06c5b3e356ecdfee7456432
aaron flores <aaronspokane@gmail.com>
2020-12-08 20:54:29 -0800

    updates

--- Web/surplus-web/src/redux/cart/cart.reducer.tsx
+++ Web/surplus-web/src/redux/cart/cart.reducer.tsx
@@ -1,9 +1,16 @@
+import CartActionTypes from './cart.types';
+
 const INITAL_STATE = {
-    hidden: true
+    cartItems: []
 };
 
 const cartReducer = (state = INITAL_STATE, action) => {
     switch(action.type) {
+        case CartActionTypes.ADD_ITEM: 
+            return {
+                ...state,
+                cartItems: [...state.cartItems, action.payload]
+            }
         default:
             return state;
     }

f048debb42f36bd02ba496bb7ceff2dd2367aa23
JeremyKrasin <33Infinity@gmail.com>
2020-12-10 13:11:03 -0800

    Updated api to handle cors

--- Api/functions/src/inventory.ts
+++ Api/functions/src/inventory.ts
@@ -1,26 +1,19 @@
-"use strict";
++"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const inventory_functions = require("firebase-functions");
 const inventory_admin = require("firebase-admin");
-//import InventoryModel from "../../../Models/Inventory";
+const cors = require("cors")({
+  origin: true,
+});
 
 exports.getByLocation = inventory_functions.https.onRequest(
   async (request: any, response: any) => {
-    const inventory = await inventory_admin
-      .firestore()
-      .collection("inventory")
-      .get();
-    //const inventoryModels: InventoryModel[] = [];
-    /* inventory.forEach(function (doc) {
-      const data = doc.data();
-      const currentInventory = InventoryModel.NewInventory(
-        data.Description,
-        data.Price,
-        data.Quantity
-      );
-      inventoryModels.push(currentInventory);
-      console.log(doc.id, "=>", doc.data());
-    }); */
-    response.send(JSON.stringify(inventory));
+    return cors(request, response, async () => {
+      const inventory = await inventory_admin
+        .firestore()
+        .collection("inventory")
+        .get();
+      response.send(JSON.stringify(inventory.docs.map((doc) => doc.data())));
+    });
   }
 );

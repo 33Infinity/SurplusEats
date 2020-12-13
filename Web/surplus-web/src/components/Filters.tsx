de60295b21a7612afc0b269cc7cb2ed3040b8882
JeremyKrasin <33Infinity@gmail.com>
2020-12-13 13:51:19 -0800

    Added caching utility

--- Web/surplus-web/src/components/Filters.tsx
+++ Web/surplus-web/src/components/Filters.tsx
@@ -1,8 +1,44 @@
 import React from "react";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import FormControl from "@material-ui/core/FormControl";
+
+class ViewBy {
+  ITEM: string = "Item";
+  VENDOR: string = "Vendor";
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  })
+);
 
 const Filters: React.FC = () => {
+  const ITEM = "Item";
+  const VENDOR = "Vendor";
+  const classes = useStyles();
+  const [viewBy, setViewBy] = React.useState("");
+  const handleViewByChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setViewBy(event.target.value as string);
+  };
   return (
     <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel>View By</InputLabel>
+        <Select value={viewBy ? viewBy : ITEM} onChange={handleViewByChange}>
+          <MenuItem value={ITEM}>Item</MenuItem>
+          <MenuItem value={VENDOR}>Vendor</MenuItem>
+        </Select>
+      </FormControl>
       <h5 className="mt-3">Price</h5>
       <div className="px-2">
         <input type="checkbox" checked={false} />

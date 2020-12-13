074302aba7985325322f7e4b3a2b55cdad5fde24
JeremyKrasin <33Infinity@gmail.com>
2020-12-13 10:19:40 -0800

    Added email endpoint

--- Api/functions/src/index.ts
+++ Api/functions/src/index.ts
@@ -1,15 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-
 exports.auth = require("./auth");
 exports.inventory = require("./inventory");
-admin.initializeApp();
-/* const auth = require("./auth");
-const inventory = require("./inventory");
-const triggers = {
-  auth,
-  inventory,
-};
-exports.trig = triggers; */
+exports.email = require("./email");
--- Api/functions/src/inventory.ts
+++ Api/functions/src/inventory.ts
@@ -2,13 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const inventory_functions = require("firebase-functions");
 const inventory_admin = require("firebase-admin");
-const cors = require("cors")({
+const inventory_cors = require("cors")({
   origin: true,
 });
 
 exports.getByLocation = inventory_functions.https.onRequest(
   async (request: any, response: any) => {
-    return cors(request, response, async () => {
+    return inventory_cors(request, response, async () => {
       const inventory = await inventory_admin
         .firestore()
         .collection("inventory")

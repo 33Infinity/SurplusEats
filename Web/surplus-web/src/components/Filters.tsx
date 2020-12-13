681134ddfc783aab70c9556c98adcbe3fe5cd5c1
JeremyKrasin <33Infinity@gmail.com>
2020-12-13 14:15:13 -0800

    Updated ui logic in filter component

--- Web/surplus-web/src/components/Filters.tsx
+++ Web/surplus-web/src/components/Filters.tsx
@@ -5,11 +5,6 @@ import InputLabel from "@material-ui/core/InputLabel";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import FormControl from "@material-ui/core/FormControl";
 
-class ViewBy {
-  ITEM: string = "Item";
-  VENDOR: string = "Vendor";
-}
-
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     formControl: {
@@ -26,28 +21,65 @@ const Filters: React.FC = () => {
   const ITEM = "Item";
   const VENDOR = "Vendor";
   const classes = useStyles();
-  const [viewBy, setViewBy] = React.useState("");
+  const [viewBy, setViewBy] = React.useState(ITEM);
   const handleViewByChange = (event: React.ChangeEvent<{ value: unknown }>) => {
     setViewBy(event.target.value as string);
+    //TODO: Tell redux to rerender either vendor list or item list
   };
+  const [sortBy, setSortBy] = React.useState("");
+  const handleSortByChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setSortBy(event.target.value as string);
+    //TODO: Tell redux to rerender how items are sorted
+  };
+
+  function GetItemSortBySelect() {
+    const PRICE = "Price";
+    const NAME = "Name";
+    return (
+      <Select value={sortBy} onChange={handleSortByChange}>
+        <MenuItem value={PRICE}>{PRICE}</MenuItem>
+        <MenuItem value={NAME}>{NAME}</MenuItem>
+      </Select>
+    );
+  }
+
+  function GetVendorSortBySelect() {
+    const DISTANCE = "Distance";
+    const NAME = "Name";
+    return (
+      <Select value={sortBy} onChange={handleSortByChange}>
+        <MenuItem value={DISTANCE}>{DISTANCE}</MenuItem>
+        <MenuItem value={NAME}>{NAME}</MenuItem>
+      </Select>
+    );
+  }
+
   return (
     <div>
       <FormControl className={classes.formControl}>
         <InputLabel>View By</InputLabel>
-        <Select value={viewBy ? viewBy : ITEM} onChange={handleViewByChange}>
-          <MenuItem value={ITEM}>Item</MenuItem>
-          <MenuItem value={VENDOR}>Vendor</MenuItem>
+        <Select value={viewBy} onChange={handleViewByChange}>
+          <MenuItem value={ITEM}>{ITEM}</MenuItem>
+          <MenuItem value={VENDOR}>{VENDOR}</MenuItem>
         </Select>
       </FormControl>
-      <h5 className="mt-3">Price</h5>
-      <div className="px-2">
-        <input type="checkbox" checked={false} />
-        <span> free</span>
-      </div>
-      <div className="px-2">
-        <input type="checkbox" checked={false} />
-        <span> less than $10</span>
-      </div>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Sort By</InputLabel>
+        {viewBy == ITEM ? GetItemSortBySelect() : GetVendorSortBySelect()}
+      </FormControl>
+      {viewBy == ITEM && (
+        <div>
+          <h5 className="mt-3">Price</h5>
+          <div className="px-2">
+            <input type="checkbox" checked={false} />
+            <span> free</span>
+          </div>
+          <div className="px-2">
+            <input type="checkbox" checked={false} />
+            <span> less than $10</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };

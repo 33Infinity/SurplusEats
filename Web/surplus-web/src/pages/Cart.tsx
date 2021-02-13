1efdc629e5ff847150a3a10b939223fb8c5815b9
JeremyKrasin <33Infinity@gmail.com>
2021-02-13 11:08:17 -0800

    Mapped cart to redux

--- Web/surplus-web/src/pages/Cart.tsx
+++ Web/surplus-web/src/pages/Cart.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
+import { selectCartItems } from "../redux/cart/cart.selectors";
 
 const Cart: React.FC = () => {
   return <div>Cart Page</div>;
 };
 
-export default Cart;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+  cartItems: selectCartItems(state),
+});
+
+export default connect(mapStateToProps)(Cart);
